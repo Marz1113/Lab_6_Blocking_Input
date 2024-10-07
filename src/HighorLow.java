e8b3d8b5103ba1ff5ea5f309dfc7a782a93b22f8
marzu <marzuqazam06@gmail.com>
2024-10-07 18:14:59 -0400

    c to f

--- src/HighorLow.java
+++ src/HighorLow.java
@@ -1,7 +1,41 @@
-public class HighorLow
-{
-    public static void main(String[] args)
-    {
-        System.out.println("hello world");
+import java.util.Random;
+import java.util.Scanner;
+
+public class HighorLow {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+        int randomNumber = random.nextInt(10) + 1;
+        int userGuess = 0;
+        boolean correctInput;
+
+        do {
+            System.out.print("Think of a number between 1-10: ");
+            if (in.hasNextInt()) {
+                userGuess = in.nextInt();
+                if (userGuess >= 1 && userGuess <= 10) {
+                    correctInput = true;
+                } else {
+                    System.out.println("Incorrect number. Must be between 1 and 10.");
+                    correctInput = false;
+                }
+            } else {
+                String trash = in.next(); // Use next() to consume the invalid input
+                System.out.println("Incorrect input: " + trash);
+                correctInput = false;
+                in.nextLine(); // Clear the invalid input from the buffer
+            }
+        } while (!correctInput);
+
+        System.out.println("Random number is: " + randomNumber);
+        if (userGuess < randomNumber) {
+            System.out.println("Your number is too small.");
+        } else if (userGuess > randomNumber) {
+            System.out.println("Your number is too big.");
+        } else {
+            System.out.println("You guessed correctly!");
+        }
+
+        in.close(); // Close the scanner
     }
 }
--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -1,8 +1,44 @@
+import java.util.Scanner;
+
 public class RectangleInfo
 {
     public static void main(String[] args)
     {
-        System.out.println("hello world");
+       Scanner in = new Scanner(System.in);
+       double width = 0, height = 0;
+       boolean correctInput;
+
+       do {
+           System.out.print("what is the width of the rectangle?: ");
+           if (in.hasNextDouble()) {
+               width = in.nextDouble();
+               correctInput = true;
+           } else {
+               String trash = in.nextLine();
+               System.out.println("invalid: " + trash);
+               correctInput = false;
+           }
+       } while (!correctInput);
+
+       do {
+           System.out.print("what is the height of the rectangle?: ");
+           if (in.hasNextDouble()) {
+               height = in.nextDouble();
+               correctInput = true;
+           } else {
+               String trash = in.nextLine();
+               System.out.println("invalid: " + trash);
+               correctInput = false;
+           }
+       } while (!correctInput);
+
+       double area = width * height;
+       double perimeter = 2 * (width + height);
+       double diagonal = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+
+        System.out.println("Area: " + area);
+        System.out.println("perimiter: " + perimeter);
+        System.out.println("diagonal: " + diagonal);
 
     }
 }
